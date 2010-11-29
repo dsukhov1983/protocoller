d8166b1535d63bddc9ba3f49dae39cdfbe9ac573
Dmitry Sukhov <dmitry.sukhov@gmail.com>
2010-11-29 22:54:25 +0300

    moved protocoller dir

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "protocoller",
+    version = "0.1",
+    url = 'http://github.com/quoter/protocoller',
+    license = 'MIT',
+    description = "An aggregator of cross country race protocols.",
+    author = 'Dmitry Sukhov',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = ['setuptools'],
+)
