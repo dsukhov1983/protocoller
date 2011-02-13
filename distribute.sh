4267bb6cdf1abf839c28bfab266ce9259dcbff8a
Dmitry Sukhov <dmitry.sukhov@gmail.com>
2011-02-13 23:35:03 +0300

    finished places map

--- distribute.sh
+++ distribute.sh
@@ -7,3 +7,4 @@ git push &&
 rsync -avz -e ssh src/protocoller/db/main $dest:devel/protocoller/src/protocoller/db/main &&
 
 ssh $dest "cd devel/protocoller && git pull &&  ~/init/protocoller restart"
+
